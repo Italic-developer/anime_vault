7d648c576c10d14f3c025955a53a5e6ef116b7c6
Patrick Umekwe <Chimeremezeumekwe@gmail.com>
2024-05-25 20:18:06 +0100

    revert

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,33 +1,30 @@
-"use client"
+"use client";
 import { featchAnime } from "@/app/action";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import AnimeCard, { AnimeProp } from "./AnimeCard";
 
-let page = 2
+let page = 1;
 
-export type AnimeCard = JSX.Element
+export type AnimeCard = JSX.Element;
 
 function LoadMore() {
-  const {ref, inView} = useInView();
-  const [data , setData] = useState<AnimeCard[]>([])
+  const { ref, inView } = useInView();
+  const [data, setData] = useState<AnimeCard[]>([]);
 
-  useEffect(
-    () => {
-      if(inView){
-        featchAnime(page)
-        .then((res) => {
-          setData([...data, ...res])
-          page++
-        })
-      }
-    },[inView , data],
-  )
+  useEffect(() => {
+    if (inView) {
+      featchAnime(page).then((res) => {
+        setData([...data, ...res]);
+        page++;
+      });
+    }
+  }, [inView, data]);
 
   return (
     <>
-    <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
         {data}
       </section>
       <section className="flex justify-center items-center w-full">
